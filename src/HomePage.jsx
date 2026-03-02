f41aeaa0516bf0bd2543b9ec3b273a9306885b08
Sayasat Kuandyk <sayasat614@gmail.com>
2026-03-02 22:32:08 +0500

    Bugs fixed

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,74 +1,70 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router";
 
-export function HomePage({onUpdate}) {
+export function HomePage({ onUpdate }) {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [randomRecipes, setRandomRecipes] = useState([]);
+  const [query, setQuery] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [isError, setIsError] = useState(false);
 
-    const [randomRecipes, setRandomRecipes] = useState([]);
-    const [query, setQuery] = useState(null);
-    const [inputValue, setInputValue] = useState("");
-    const [isError, setIsError] = useState(false);
-
-    useEffect(() => {
+  useEffect(() => {
     const getRandomRecipes = async () => {
-        try {
-            setIsError(false);
-            const url = "https://www.themealdb.com/api/json/v1/1/random.php";
+      try {
+        setIsError(false);
+        const url = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-            const responses = await Promise.all([
-                fetch(url),
-                fetch(url),
-                fetch(url)
-            ]);
+        const responses = await Promise.all([
+          fetch(url),
+          fetch(url),
+          fetch(url),
+        ]);
 
-            responses.forEach(res => {
-                if (!res.ok) throw new Error;
-            });
+        responses.forEach((res) => {
+          if (!res.ok) throw new Error();
+        });
 
-            const data = await Promise.all(responses.map(res => res.json()));
-            const meals = data.map(item => item.meals[0]);
-            console.log(meals)
-            setRandomRecipes(meals);
-        } catch (error) {
-            setIsError(true);
-        }
+        const data = await Promise.all(responses.map((res) => res.json()));
+        const meals = data.map((item) => item.meals[0]);
+        console.log(meals);
+        setRandomRecipes(meals);
+      } catch (error) {
+        setIsError(true);
+      }
     };
 
     getRandomRecipes();
-}, []);
+  }, []);
 
-    const searchMeal = () =>{
-        if(inputValue.trim()) setQuery(inputValue);
+  const searchMeal = () => {
+    if (inputValue.trim()) setQuery(inputValue);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+        const response = await fetch(url);
+
+        if (!response.ok) throw new Error();
+
+        const data = await response.json();
+
+        const foundMeal = data.meals[0];
+
+        onUpdate(foundMeal);
+
+        navigate("/recipe", { state: { recipe: foundMeal } });
+      } catch (error) {
+        setIsError(true);
+      }
     };
 
-    useEffect(()=>{
-        const fetchData = async() => {
-            try{
-                setIsLoading(true);
-                setIsError(false);
-                const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
-                const response = await fetch(url);
-
-                if(!response.ok) throw new Error;
-
-                const data = await response.json();
-
-                const foundMeal = data.meals[0];
-
-                onUpdate(foundMeal);
-
-                navigate('/recipe', {state:{recipe: foundMeal}})
-                
-            }
-
-            catch (error){
-                setIsError(true);
-            }
-        }
-
-        fetchData();
-    },[query]);
+    fetchData();
+  }, [query]);
 
   return (
     <>
@@ -76,38 +72,45 @@ export function HomePage({onUpdate}) {
       <header>
         <div className="header-content">
           <h1>YummyRecipe</h1>
-          <form className="search-box" onSubmit={(e) => { e.preventDefault(); searchMeal(); }}>
-                <input 
-                    type="text" 
-                    placeholder="Enter a meal name" 
-                    onChange={(e) => setInputValue(e.target.value)}
-                />
-                <button type="submit" className="btn-primary">Find</button>
-            </form>
+          <form
+            className="search-box"
+            onSubmit={(e) => {
+              e.preventDefault();
+              searchMeal();
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Enter a meal name"
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button type="submit" className="btn-primary">
+              Find
+            </button>
+          </form>
         </div>
       </header>
 
       <main className="container">
         <section className="recipe-grid">
           {randomRecipes.map((recipe, index) => (
-                <Link 
-                key={recipe?.idMeal || index} 
-                to="/recipe" 
-                state={{ recipe }}
-                className="recipe-card"
-                >
-                <div className="card-image">
-                    <img src={recipe?.strMealThumb} alt={recipe?.strMeal} />
-                </div>
-                <div className="card-info">
-                    <span className="category">{recipe?.strCategory}</span>
-                    <h3>{recipe?.strMeal}</h3>
-                </div>
-                </Link>
-            ))}
+            <Link
+              key={recipe?.idMeal || index}
+              to="/recipe"
+              state={{ recipe }}
+              className="recipe-card"
+            >
+              <div className="card-image">
+                <img src={recipe?.strMealThumb} alt={recipe?.strMeal} />
+              </div>
+              <div className="card-info">
+                <span className="category">{recipe?.strCategory}</span>
+                <h3>{recipe?.strMeal}</h3>
+              </div>
+            </Link>
+          ))}
         </section>
       </main>
-
     </>
   );
 }
